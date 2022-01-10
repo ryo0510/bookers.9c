c73a38899faaa2de10978841685ca304c0ae1fe8
EC2 Default User <ec2-user@ip-172-31-42-65.ec2.internal>
2022-01-10 06:19:12 +0000

    (start)

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,65 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def index
+    @book = Book.new
+    @groups = Group.all
+  end
+
+  def show
+    @book = Book.new
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    @group.users << current_user # @group.usersにcurrent_user追加
+    if @group.save
+      redirect_to groups_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path
+    else
+      render "edit"
+    end
+  end
+
+  def join
+    @group = Group.find(params[:group_id])
+    @group.users << current_user # @group.usersにcurrent_user追加
+    redirect_to  groups_path
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user) # current_userは、@group.usersから消されるという記述。
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :introduction, :image)
+  end
+
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path
+    end
+  end
+end
